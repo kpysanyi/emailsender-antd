2f8bcb62690952e28636698ae2983e3a94336189
techqulxpert <tech.qulxpert@gmail.com>
2019-03-08 22:34:16 +0200

    Updates

---
+++ precache-manifest.61a503d0ceddd3f0b8e8cc6b4b4f2e23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e75c3e0167e9bda0d24",
+    "url": "/emailsender-antd/static/js/runtime~main.39e5a392.js"
+  },
+  {
+    "revision": "214c572cf627fd2497a4",
+    "url": "/emailsender-antd/static/js/main.f50fdd3f.chunk.js"
+  },
+  {
+    "revision": "a90d35a7536e1d177b16",
+    "url": "/emailsender-antd/static/js/2.71155649.chunk.js"
+  },
+  {
+    "revision": "214c572cf627fd2497a4",
+    "url": "/emailsender-antd/static/css/main.81112465.chunk.css"
+  },
+  {
+    "revision": "a90d35a7536e1d177b16",
+    "url": "/emailsender-antd/static/css/2.b84a2ce3.chunk.css"
+  },
+  {
+    "revision": "f9756ea8ece9a5e526db9c8f532a35f0",
+    "url": "/emailsender-antd/index.html"
+  }
+];
